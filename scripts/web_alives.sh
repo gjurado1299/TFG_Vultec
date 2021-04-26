252632f71b7b317748d74de0cb7b1ff2677cf1bf
gjurado1299 <gjurado1299@gmail.com>
2021-04-26 17:35:36 +0200

    Clean

--- scripts/web_alives.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PATH_RES="/home/gonxo/Vultec/scans"
-TLD=$1
-cat $PATH_RES/$TLD/subdomains.txt | httpx -no-color -status-code -title -content-length -ip -cname -web-server -tech-detect -silent -ports \
-    80,443,8000,8080 -timeout 5 -threads 300 -json > $PATH_RES/$TLD/webalives.txt
